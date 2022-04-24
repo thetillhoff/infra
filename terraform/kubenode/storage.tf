3f7740a884ecf34659185e365c80877649e1b26e
Till Hoffmann <till.hoffmann@thetillhoff.de>
2022-04-24 17:00:37 +0200

    fix typo

--- terraform/kubenode/storage.tf
+++ terraform/kubenode/storage.tf
@@ -1,6 +1,6 @@
 # Create volume
 resource "hcloud_volume" "kubenode_volume" {
-  service_id = hcloud_server.kubenode.id
+  server_id = hcloud_server.kubenode.id
 
   name = "vol-${hcloud_server.kubenode.name}"
   size = var.storagesize
