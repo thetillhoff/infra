bbc237f23105ec938ebe4a069ae5c919ca1e8118
Till Hoffmann <till.hoffmann@thetillhoff.de>
2022-04-26 00:01:14 +0200

    adding port 6443 to loadbalancer

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -28,3 +28,18 @@ resource "hcloud_load_balancer_service" "https" {
     retries = 3
   }
 }
+
+resource "hcloud_load_balancer_service" "https" {
+  load_balancer_id = hcloud_load_balancer.kubernetes.id
+  protocol = "tcp"
+  listen_port = 6443
+  destination_port = 6443
+
+  health_check {
+    protocol = "tcp"
+    port = 6443
+    interval = 5
+    timeout = 5
+    retries = 3
+  }
+}
