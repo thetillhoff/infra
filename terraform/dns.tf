84e2bb29ddf0585bbb7cd6b9213868dda1b1f899
Till Hoffmann <till.hoffmann@thetillhoff.de>
2022-05-03 23:37:35 +0200

    more fixes

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,7 +2,7 @@
 resource "cloudflare_record" "k8s" {
   zone_id = "94d9f474ce48a61513a68744b663f5e5"
   name = "k8s.${var.ROOT_DOMAIN}"
-  value = "${hcloud_server.kubenode.ipv4}"
+  value = "${hcloud_server.kubenode.ipv4_address}"
   type = "A"
   ttl = 3600
 }
@@ -11,7 +11,7 @@ resource "cloudflare_record" "k8s" {
 resource "cloudflare_record" "root" {
   zone_id = "94d9f474ce48a61513a68744b663f5e5"
   name = "${var.ROOT_DOMAIN}"
-  value = "${hcloud_server.kubenode.ipv4}"
+  value = "${hcloud_server.kubenode.ipv4_address}"
   type = "A"
   ttl = 3600
 }
@@ -20,7 +20,7 @@ resource "cloudflare_record" "root" {
 resource "cloudflare_record" "link" {
   zone_id = "94d9f474ce48a61513a68744b663f5e5"
   name = "link.${var.ROOT_DOMAIN}"
-  value = "${hcloud_server.kubenode.ipv4}"
+  value = "${hcloud_server.kubenode.ipv4_address}"
   type = "A"
   ttl = 3600
 }
--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -1,6 +1,6 @@
 resource "local_file" "ansible_inventory" {
   content = templatefile("inventory.ini.tpl", {
-    kubenodes = [hcloud_server.kubenode.public_ipv4_address]
+    kubenodes = [hcloud_server.kubenode.ipv4_address]
   })
   filename = "../inventory.ini"
 }
