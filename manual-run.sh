45edcf46b7409f294d1e948e772e332b498141b3
Till Hoffmann <till.hoffmann@thetillhoff.de>
2022-09-25 20:13:23 +0200

    adding first draft of manual run script

---
+++ manual-run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+apt install python3-pip -y
+
+python3 -m pip install ansible
+
+ansible-galaxy install -r "ansible/requirements.yaml"
+
+CONTROL_PLANE_ENDPOINT="${hostname}" \
+  CLUSTER_NAME="${hostname}" \
+  ansible-playbook "ansible/${hostname}.yaml" -i "ansible/inventory.yaml" -bK
