cd5e7f25bbbfd0de9e1855688d3942648a4375e4
Till Hoffmann <till.hoffmann@thetillhoff.de>
2022-09-25 20:20:11 +0200

    fix hostname references in manual-run.sh

--- manual-run.sh
+++ manual-run.sh
@@ -6,6 +6,6 @@ python3 -m pip install ansible
 
 ansible-galaxy install -r "ansible/requirements.yaml"
 
-CONTROL_PLANE_ENDPOINT="${hostname}" \
-  CLUSTER_NAME="${hostname}" \
-  ansible-playbook "ansible/${hostname}.yaml" -i "ansible/inventory.yaml" -bK
+CONTROL_PLANE_ENDPOINT="$(hostname)" \
+  CLUSTER_NAME="$(hostname)" \
+  ansible-playbook "ansible/$(hostname).yaml" -i "ansible/inventory.yaml" -bK
