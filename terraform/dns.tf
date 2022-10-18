a3834425a61855156d11691c57d0eb6151079806
Till Hoffmann <till.hoffmann@thetillhoff.de>
2022-10-18 21:13:30 +0200

    added pihole dns entry

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -42,3 +42,12 @@ resource "cloudflare_record" "umami" {
   type    = "A"
   ttl     = 3600
 }
+
+# pihole
+resource "cloudflare_record" "pihole" {
+  zone_id = "94d9f474ce48a61513a68744b663f5e5"
+  name    = "pihole.thetillhoff.de"
+  value   = hcloud_server.kubenode.ipv4_address
+  type    = "A"
+  ttl     = 3600
+}
