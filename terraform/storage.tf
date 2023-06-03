df6dfaefd3429019ceb0881c23b1734c132a8bba
Till Hoffmann <till@thetillhoff.de>
2023-06-03 14:42:35 +0200

    Add external storage to kubenode

---
+++ terraform/storage.tf
@@ -0,0 +1,13 @@
+# Create storage
+resource "hcloud_volume" "kubenode_storage" {
+  name      = "kubenode"
+  size      = 10
+  location    = var.location
+  format    = "ext4"
+}
+
+resource "hcloud_volume_attachment" "kubenode_storagemount" {
+  volume_id = hcloud_volume.kubenode_storage.id
+  server_id = hcloud_server.kubenode.id
+  automount = true
+}
