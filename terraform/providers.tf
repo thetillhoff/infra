d7aceff3aa6dc7653091624e86c3888013379570
Till Hoffmann <till@thetillhoff.de>
2023-06-03 14:33:24 +0200

    upgrade provider versions

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,12 +10,12 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.35.1"
+      version = "1.39.0"
     }
 
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "3.30.0"
+      version = "4.7.1"
     }
   }
 }
