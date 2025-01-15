83545b5074ecde68fe6635d983898b656a9bae61
Till Hoffmann <till@thetillhoff.de>
2025-01-15 01:25:50 +0100

    Add kubernetes provider auth

--- terraform/kubenodes/providers.tf
+++ terraform/kubenodes/providers.tf
@@ -85,3 +85,11 @@ provider "helm" {
     cluster_ca_certificate = base64decode(talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.ca_certificate)
   }
 }
+
+provider "kubernetes" {
+  host = talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.host
+
+  client_certificate     = base64decode(talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.client_certificate)
+  client_key             = base64decode(talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.client_key)
+  cluster_ca_certificate = base64decode(talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.ca_certificate)
+}
