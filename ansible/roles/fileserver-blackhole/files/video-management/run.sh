4fe18f3422ca58bb99b84e595d2a76196e3c7962
Till Hoffmann <till@thetillhoff.de>
2025-10-25 23:05:18 +0200

    fix path

--- ansible/roles/fileserver-blackhole/files/video-management/run.sh
+++ ansible/roles/fileserver-blackhole/files/video-management/run.sh
@@ -1,7 +1,7 @@
 docker run \
   --rm \
   -v ./script.sh:/script.sh \
-  -v /mnt/cold/public:/data \
+  -v /mnt/cold/public/videos:/data \
   -w /data \
   --entrypoint 'bash' \
   jrottenberg/ffmpeg:7.1-ubuntu-edge \
